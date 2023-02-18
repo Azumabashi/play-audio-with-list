3178f50afa5077e045c2fb2b60182f54cc1a152e
Azumabashi <>
2023-02-18 09:51:36 +0900

    get configurations

--- main.nim
+++ main.nim
@@ -1,13 +1,22 @@
 import parseopt
 import os
 
-var p = initOptParser()
+var 
+  p = initOptParser()
+  voiceDirectory = ""
+  questionFile = ""
+  regexStr = ""
 while true:
   p.next()
   case p.kind
   of cmdEnd:
     break
-  of cmdArgument:
-    continue
+  of cmdLongOption:
+    if p.key == "voice":
+      voiceDirectory = p.val
+    elif p.key == "question":
+      questionFile = p.val
+    elif p.key == "regex":
+      regexStr = p.val
   else:
     continue
