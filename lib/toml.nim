069d309ad7744271daff5c6b10145b60cbfe126e
Azumabashi <>
2023-02-18 09:07:59 +0900

    change interface of proc readFromToml

--- lib/toml.nim
+++ lib/toml.nim
@@ -2,11 +2,11 @@ import parsetoml
 import re
 import types
 
-proc readFromToml*(option: var Options, path: string) = 
+proc readFromToml*(path: string): Options = 
   let parsedToml = parseToml.parseFile(path)["config"]
   if parsedToml.hasKey("path"):
-    option.dirPath = parsedToml["path"].getStr()
+    result.dirPath = parsedToml["path"].getStr()
   if parsedToml.hasKey("interval"):
-    option.interval = parsedToml["interval"].getInt()
+    result.interval = parsedToml["interval"].getInt()
   if parsedToml.hasKey("regex"):
-    option.regex = re(parsedToml["regex"].getStr())
+    result.regex = re(parsedToml["regex"].getStr())
